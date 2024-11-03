2df63e9735af268ce93bace160b6745526a546b9
dkoldaev <dkoldyaev@gmai.com>
2024-11-03 17:49:08 +0400

    build + analyze

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { visualizer } from 'rollup-plugin-visualizer'
+
+const isAnalyzeMode = process.env.ANALYZE === 'true';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,5 +10,12 @@ export default defineConfig({
   build: {
     outDir: './docs'
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    ...(isAnalyzeMode ? [visualizer({
+      open: true, // Открыть визуализатор в браузере после сборки
+      filename: 'stats.html', // Имя файла для визуализации
+      gzipSize: true, // Показать размеры после сжатия gzip
+    })] : [])
+  ],
 })
