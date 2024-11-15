bb57ec77ec6af804a967a46b850cbc1c17f0e5f3
dkoldaev <dkoldyaev@gmai.com>
2024-11-15 16:29:50 +0400

    fix footer height

--- src/components/footer.component.tsx
+++ src/components/footer.component.tsx
@@ -5,7 +5,7 @@ import { memo } from "react";
 const link = <a href="https://forum.awd.ru/viewtopic.php?p=11124426&sid=c9f23ee4c5284ff486bf8bb82ca473e9#p11124426" target="_blank" rel="nofollow">https://forum.awd.ru/</a>;
 
 export const Footer = memo(() => {
-  return <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '40px', padding: '1em' }}>
+  return <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', padding: '1em' }}>
     <Typography variant="body1" align="center" sx={{ fontSize: '12px', textWrap: 'balance' }} gutterBottom>
       Сервис использует данные с сайта info.midpass.ru, и отображаемая информация полностью зависит от данных, предоставленных МИД РФ.
       <br />
